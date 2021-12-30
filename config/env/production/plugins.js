c716ff275c0c85e235001bdc66b9c591014a3a26
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2021-12-30 21:07:30 +0100

    Add new aws s3 config settings object

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,23 +1,15 @@
 module.exports = ({ env }) => ({
   // ...
   upload: {
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID_OWN"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION_OWN"),
-      params: {
-        Bucket: env("AWS_BUCKET"),
-      },
-    },
-  },
-  graphql: {
     config: {
-      endpoint: "/graphql",
-      playgroundAlways: true,
-      apolloServer: {
-        tracing: false,
-        introspection: true,
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
       },
     },
   },
