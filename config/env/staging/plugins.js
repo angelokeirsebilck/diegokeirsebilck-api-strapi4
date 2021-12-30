437b5967f9a399ea79e9934f88e1b8db85cb39cd
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2021-12-30 21:17:39 +0100

    Update upload config

--- config/env/staging/plugins.js
+++ config/env/staging/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        accessKeyId: env("AWS_ACCESS_KEY_ID_OWN"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
+        region: env("AWS_REGION_OWN"),
         params: {
           Bucket: env("AWS_BUCKET"),
         },
